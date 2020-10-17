5dd507cbe86e93338d009770b7066a01ddab06e4
btme0011 <71667635+btme0011@users.noreply.github.com>
2020-10-18 00:39:34 +0530

    Update testingCode.cpp

--- testingCode.cpp
+++ testingCode.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
-
 using namespace std;
- 
- 
-
+#include <ext/pb_ds/assoc_container.hpp> 
+using namespace __gnu_pbds; 
 
 //c++ templte for help.......................................................................................................................................................
 
@@ -11,24 +9,24 @@ typedef long long ll;
 
 template<typename... T>
 void print1(T... t){
-	((cerr<<t<<" "),...);
+    ((cout<<t<<" "),...);
 }
 
 template<typename... T>
 void print2(T... t){
-		vector<vector<ll>> ans={t...};
-		for(ll i=0;i<(ll)ans.size();i++){
-			if(i!=0) cerr<<"\t\t";
-			else cerr<<"\t";
-			for(ll j=0;j<(ll)ans[i].size();j++){
-				cerr<<ans[i][j]<<" ";
-			}
-			if(i!=(ll)ans.size()-1)
-			cerr<<endl;
-		}
-}	
-#define deb(...) {cerr<<" [ "#__VA_ARGS__<<" : "; print1(__VA_ARGS__); cerr<<"]"<<endl; }
-#define vdeb(...) {cerr<<" [ "#__VA_ARGS__<<" : "; print2(__VA_ARGS__); cerr<<"]"<<endl;}
+        vector<vector<ll>> ans={t...};
+        for(ll i=0;i<(ll)ans.size();i++){
+            if(i!=0) cout<<"\t\t";
+            else cout<<"\t";
+            for(ll j=0;j<(ll)ans[i].size();j++){
+                cout<<ans[i][j]<<" ";
+            }
+            if(i!=(ll)ans.size()-1)
+            cout<<endl;
+        }
+}    
+#define deb(...) {cout<<" [ "#__VA_ARGS__<<" : "; print1(__VA_ARGS__); cout<<"]"<<endl; }
+#define vdeb(...) {cout<<" [ "#__VA_ARGS__<<" : "; print2(__VA_ARGS__); cout<<"]"<<endl;}
 
 // to return everything in string
 #define ret(...) {stringstream returnn;returnn<<__VA_ARGS__;return returnn.str();}
@@ -36,19 +34,19 @@ void print2(T... t){
 stringstream retvec;
 template<typename... T>
 void returnvector(T... t){
-		vector<vector<ll>> ans={t...};
-		for(ll i=0;i<(ll)ans.size();i++){
-			for(ll j=0;j<(ll)ans[i].size();j++){
-				retvec<<ans[i][j];
-				if(j!=(ll)ans[i].size()-1)
-					retvec<<" ";
-			}
-			if(i!=(ll)ans.size()-1)
-				retvec<<endl;
-		}
+        vector<vector<ll>> ans={t...};
+        for(ll i=0;i<(ll)ans.size();i++){
+            for(ll j=0;j<(ll)ans[i].size();j++){
+                retvec<<ans[i][j];
+                if(j!=(ll)ans[i].size()-1)
+                    retvec<<" ";
+            }
+            if(i!=(ll)ans.size()-1)
+                retvec<<endl;
+        }
 }
-#define vret(...) {retvec.clear();returnvector(__VA_ARGS__);return retvec.str()};
-	
+#define vret(...) {retvec.clear();returnvector(__VA_ARGS__);return retvec.str();}
+    
 
 
 //for loop
@@ -59,25 +57,35 @@ void returnvector(T... t){
 //to print vector element 
 template<typename T>
 void printV(T vec){
-	fo(i,vec.size()) cout<<vec[i]<<" "<<flush;
-	cout<<endl;
-	}
-	
+    fo(i,vec.size()) cout<<vec[i]<<" "<<flush;
+    cout<<endl;
+    }
+    
  
-#define M 998244353
+#define M ll(998244353)
 const ll INF=ll(1e9+5);
  
 //iterate map
 template<typename T>
 void itR(T map){
-	 for(auto itr=map.begin();itr!=map.end();itr++){
-				deb(itr->first,itr->second);
-			}
-	cout<<endl;
+     for(auto itr=map.begin();itr!=map.end();itr++){
+                deb(itr->first,itr->second);
+            }
+    cout<<endl;
 }
+
+typedef tree<
+ll,
+null_type,
+greater<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+pods;
+ 
 
 //c++ template for help.....................................................................................................................................................
 
+// CALM DOWN : - )  
 
 string solve(){
 	
@@ -95,20 +103,31 @@ string solve(){
 	
 	
 	
-	ret("");
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+ret("");
 }
 
 int main() {
-	cout<<setprecision(10);
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	//ll testCase;
-	//cin>>testCase;
-	//for(ll tt=0;tt<testCase;tt++){
-		string ans1=solve();
-		if(ans1!="")
-			cout<<ans1<<endl;
-	//}
-	return 0;
+    cout<<setprecision(10);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll testCase;
+    cin>>testCase;
+    for(ll tt=0;tt<testCase;tt++){
+        string ans1=solve();
+        if(ans1!="")
+            cout<<ans1<<endl;
+    }
+    return 0;
 }
